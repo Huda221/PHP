29e0fa8831b374a49ae77e2d0bd7848d9c01d6a9
Huda Zahid <142784857+Huda221@users.noreply.github.com>
2025-04-15 21:56:23 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Commerce Website</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="bootsrtrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="bootsrtrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootsrtrap/js/bootstrap.min.js">
+    <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:wght@700&family=Inter:wght@300&family=Montserrat:wght@200&family=Nanum+Gothic&family=Nunito+Sans:opsz,wght@6..12,300&family=Oswald&family=Roboto+Condensed:wght@700&family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/8e351b2eb4.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container-fluid head">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 col-md-3">
+                    <div class="logo">
+                        <img src="images/logo.png">
+                    </div>
+                </div>
+                <div class="col-6 col-md-5">
+                    <div class="search">
+                        <input type="text" placeholder="Search Here">
+                        <button>
+                            <img src="images/search.svg" alt="">
+                        </button>
+                    </div>
+                </div>
+                <div class="col-6 col-md-4">
+                    <ul class="menu">
+                        <li><a href="#products">Products</a></li>
+                        <li><a href="#sell">Sell</a></li>
+                        <li><a href="record">Record</a></li>
+                        <li><a href="admin/login page/login.php">Admin</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid second">
+        <div class="container">
+            <div class="row category">
+                <div class="col-12 col-md-12">
+                    <ul>
+                        <li><a href="#category" class="first"></i>Categories</a></li>
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#stores">Stores</a></li>
+                        <li><a href="#refer">Refer and earn</a></li>
+                        <li><a href="#blog">Blog</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid third">
+        <div class="container">
+            <div class="row thirdrow">
+                <div class="col-12 col-md-6">
+                    <img src="images/appliances.jpeg" alt="">
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="text">
+                        <h1>Electronics</h1>
+                        <hr>
+                        <h1>Up to 50% Off</h1>
+                        <p>On Waching Machines, Refrigerators, Microwave Ovens, Dryers, Vacuum Cleaners and many more...</p>
+                        <button>SHOP NOW!</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid four">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 col-md-3">
+                    <img src="images/shopifylogo.jpg" alt="">
+                </div>
+                <div class="col-6 col-md-3">
+                    <img src="images/sites-like-aliexpress.jpg" alt="" class="express">
+                </div>
+                <div class="col-6 col-md-3">
+                    <img src="images/amazon 1.png" alt="">
+                </div>
+                <div class="col-6 col-md-3">
+                    <img src="images/eBay-1.jpg" alt="" class="ebay">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid five">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 col-md-6">
+                    <div class="text">
+                        <h1>POPULAR CATEGORIES</h1>
+                        <button> 
+                            <P>VIEW ALL <i class="fa-solid fa-arrows-turn-right"></i></P>
+                        </button>
+                        
+                    </div>
+                </div>
+                <div class="col-6 col-md-3">
+                    <div class="card cards">
+                        <p><i class="fa-solid fa-bag-shopping"></i> women's fashion</p>
+                    </div>
+                    <div class="card cards">
+                        <p><i class="fa-solid fa-bag-shopping"></i> women's fashion</p>
+                    </div>
+                    <div class="card cards">
+                        <p><i class="fa-solid fa-bag-shopping"></i> women's fashion</p>
+                    </div>
+                    <div class="card cards">
+                        <p><i class="fa-solid fa-bag-shopping"></i> women's fashion</p>
+                    </div>
+                </div>
+                <div class="col-6 col-md-3">
+                    <div class="card cards">
+                        <p><i class="fa-solid fa-shoe-prints"></i> Men's Fashion</p>
+                    </div>
+                    <div class="card cards">
+                        <p><i class="fa-solid fa-shoe-prints"></i> Men's Fashion</p>
+                    </div>
+                    <div class="card cards">
+                        <p><i class="fa-solid fa-shoe-prints"></i> Men's Fashion</p>
+                    </div>
+                    <div class="card cards">
+                        <p><i class="fa-solid fa-shoe-prints"></i> Men's Fashion</p>
+                    </div>
+
+                </div>
+
+               
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid six">
+        <div class="container">
+            <div class="row">
+                <div class="col-9 col-md-9">
+                    <p>Latest Deals and Coupons Today!</p>
+                </div>
+                <div class="col-3 col-md-3">
+                    <div class="icons">
+                        <i class="fa-solid fa-less-than"></i>
+                        <i class="fa-solid fa-greater-than"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-4">
+                    <img src="images/sale.jpeg" alt="">
+                </div>
+                <div class="col-12 col-md-4">
+                    <img src="images/sale 2.jpeg" alt="">
+                </div>
+                <div class="col-12 col-md-4">
+                    <img src="images/sale 3.jpeg" alt="" class="deal">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
